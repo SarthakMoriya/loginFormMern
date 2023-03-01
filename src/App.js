fb5d90824b497dc1d97817609a1aa53adf16c956
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-03-01 21:49:47 +0530

    loginMern

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Username, Password, Recovery, Profile, Register, Reset, PageNotFound } from './components'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Username/>} />
+                <Route path='/password' element={<Password/>} />
+                <Route path='/recovery' element={<Recovery/>} />
+                <Route path='/profile' element={<Profile/>} />
+                <Route path='/register' element={<Register/>} />
+                <Route path='/reset' element={<Reset/>} />
+                <Route path='*' element={<PageNotFound/>} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Username.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../index.css'
+const Username = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="title flex flex-col items-center">
+            <h4 className='text-5xl font-bold'>Hello Again!</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-5">Explore More by connecting with us.
+            </span>
+          </div>
+          <form>
+            
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Username
